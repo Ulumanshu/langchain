aae51ef7fb350dcc9d833b666a6c3f69352cf7ac
karksm <karolis.kesminas@telesoftas.com>
2024-11-06 18:52:03 +0200

    chore: lint issues

--- libs/community/langchain_community/tools/oxylabs_search/tool.py
+++ libs/community/langchain_community/tools/oxylabs_search/tool.py
@@ -37,7 +37,7 @@ class OxylabsSearchRun(BaseTool):
         " a geo_location string to enhance result accuracy. "
         "The output is a compiled, formatted summary of query results. "
     )
-    args_schema: Type[OxylabsSearchQueryInput] = OxylabsSearchQueryInput
+    args_schema: Type[BaseModel] = OxylabsSearchQueryInput
 
     def _run(
         self,
@@ -79,7 +79,7 @@ class OxylabsSearchResults(BaseTool):
         " a geo_location string to enhance result accuracy. "
         "The output is a JSON array of response page objects. "
     )
-    args_schema: Type[OxylabsSearchQueryInput] = OxylabsSearchQueryInput
+    args_schema: Type[BaseModel] = OxylabsSearchQueryInput
 
     def _run(
         self,
